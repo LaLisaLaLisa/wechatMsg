14197e08d80006fc093bf52028149c0ce131f02b
Betty Zhong <61925024+minionkevin@users.noreply.github.com>
2022-08-24 20:58:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,16 @@ def get_birthday(birthday, year, today):
         birth_day = str(birth_date.__sub__(today)).split(" ")[0]
     return birth_day
  
- 
+def get_num():
+      url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
+    week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
+    year = localtime().tm_year
+    month = localtime().tm_mon
+    day = localtime().tm_mday
+    today = datetime.date(datetime(year=year, month=month, day=day))
+    week = week_list[today.isoweekday() % 7]
+    
+    
 def get_ciba_en():
   if(note_en == "1"):
         url = "http://open.iciba.com/dsapi/"
@@ -113,6 +122,7 @@ def get_ciba_en():
         r = get(url, headers=headers)
         note_en = r.json()["content"]
    elif(note_en == "2"):
+    num = get_num()
         if num == 0:
             note_en = "一定要好好享受周末的最后一天！不要老是想着明天要上班了就emo,珍惜今天！"
         elif num == 1:
@@ -150,25 +160,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
     day = localtime().tm_mday
     today = datetime.date(datetime(year=year, month=month, day=day))
     week = week_list[today.isoweekday() % 7]
-    
-    num = today.isoweekday() % 7
-    # 注释如果要自己手动改
-    if num == 0:
-        note_en = "一定要好好享受周末的最后一天！不要老是想着明天要上班了就emo,珍惜今天！"
-    elif num == 1:
-        note_en = "又要开始上班啦，又有钱钱拿"
-    elif num == 2:
-       note_en = "今天是个好日子，也不知道昨天的班上的怎么样。希望今天也有好运气" 
-    elif num == 3:
-       note_en = "马上一周就要过去一半啦，今天回家吃点香香的"
-    elif num ==4:
-       note_en = "再挺两天就周末啦,周四也是要好好上班的一天"
-    elif num ==5:
-       note_en = "周五啦！再过几个小时就可以享受周末咯"
-    elif num == 6:
-       note_en = "美好的一天开始啦，你这个小猪仔现在肯定还没起床"
- 
-    
+
        
     # 获取在一起的日子的日期格式
     love_year = int(config["love_date"].split("-")[0])
