ae565640f0b17ad8341efe50d4aa76f7603ba0ed
Betty Zhong <61925024+minionkevin@users.noreply.github.com>
2022-08-24 20:50:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -285,8 +285,8 @@ if __name__ == "__main__":
     
     if note_ch == "":
      note_ch = get_ciba_ch()
-    if note_en == "":
-     note_en = get_ciba_en()
+   # if note_en == "":
+    # note_en = get_ciba_en()
      
     # 公众号推送消息
     for user in users:
