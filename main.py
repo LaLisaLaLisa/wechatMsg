90f526d370498d99c9ec309209a76142412c65cc
Betty Zhong <61925024+minionkevin@users.noreply.github.com>
2022-08-24 20:19:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -139,18 +139,16 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         note_en = "一定要好好享受周末的最后一天！不要老是想着明天要上班了就emo,珍惜今天！"
     elif(today.isoweekday() % 7 == 1):
         note_en = "又要开始上班啦，又有钱钱拿"
-  # elif if today.isoweekday() % 7 == 1: 
-   #     note_en = "又要开始上班啦，又有钱钱拿了"
-  # elif today.isoweekday() % 7 == 2:
-   #    note_en = "今天是个好日子，也不知道昨天的班上的怎么样。希望今天也有好运气" 
-   #elif today.isoweekday() % 7 == 3:
-  #     note_en = "马上一周就要过去一半啦，今天回家吃点香香的"
-   #elif today.isoweekday() % 7 ==4:
-  #      note_en = "再挺两天就周末啦,周四也是要好好上班的一天"
-  # elif today.isoweekday() % 7 ==5:
-      #  note_en = "周五啦！再过几个小时就可以享受周末咯"
-#   elif today.isoweekday() % 7 == 6:
-     #   note_en = "美好的一天开始啦，你这个小猪仔现在肯定还没起床"
+    elif(today.isoweekday() % 7) == 2:
+       note_en = "今天是个好日子，也不知道昨天的班上的怎么样。希望今天也有好运气" 
+    elif today.isoweekday() % 7 == 3:
+       note_en = "马上一周就要过去一半啦，今天回家吃点香香的"
+    elif today.isoweekday() % 7 ==4:
+       note_en = "再挺两天就周末啦,周四也是要好好上班的一天"
+    elif today.isoweekday() % 7 ==5:
+       note_en = "周五啦！再过几个小时就可以享受周末咯"
+    elif today.isoweekday() % 7 == 6:
+       note_en = "美好的一天开始啦，你这个小猪仔现在肯定还没起床"
  
     
        
