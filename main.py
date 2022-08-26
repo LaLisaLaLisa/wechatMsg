aa6b0fba6c434c3bad46272fe84057ecc263e0f8
Betty Zhong <61925024+minionkevin@users.noreply.github.com>
2022-08-26 13:14:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -164,8 +164,8 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
     newyork_time = timezone(timedelta(hours=-4))
     time_newyork = utc_time.astimezone(newyork_time)
     
-    extra_msg = str(time_newyork.date())
-    
+    #extra_msg = str(time_newyork.date())
+    extra_msg = str(day) + str(today)
     
 # 根据星期几 发送不同的配置句子
     note_en = get_today_day(today.isoweekday() % 7,note_en)
