5c7d0805eb103b8993e584fb8e9791d4a292715e
Betty Zhong <61925024+minionkevin@users.noreply.github.com>
2022-08-26 12:06:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def get_ciba_ch():
     return note_ch
 
 
-def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
+def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en,tester):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
@@ -209,6 +209,10 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
             },
             "note_ch": {
                 "value": note_ch,
+                "color": get_color()
+            },
+            "tester": {
+                "value": tester,
                 "color": get_color()
             }
         }
