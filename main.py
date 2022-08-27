1a0b3c957d8d1077a8b274bf029be003b5634852
Betty Zhong <61925024+minionkevin@users.noreply.github.com>
2022-08-27 00:34:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ def get_greet_note(greet_note, time, isDayOff):
         greet_note = "中午啦，有没有好好吃饭鸭"
     elif 17 <= time & time <= 18:
         greet_note = "终于休息咯，快回家躺在床上当个咸鱼吧"
-    elif (23 <= time | time <= 1) & isDayOff:
+    elif (23 <= time | time <= 1) & isDayOff == True:
         greet_note = "快起来玩游戏啦，当然也别忘了要好好休息，周末愉快，努力把把吃鸡"
     elif (23 <= time | time <= 1) & isDayOff == False:
         greet_note = "该睡觉啦，放下手机订好闹钟，明天还是打工人的一天，晚安"
@@ -201,7 +201,8 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         greet_note = get_greet_note(greet_note, curr_time.hour, False)
 
     # 根据星期几 发送不同的配置句子
-    extra_msg = get_today_day(today.isoweekday() % 7, extra_msg)
+    # extra_msg = get_today_day(today.isoweekday() % 7, extra_msg)
+    extra_msg = str(today.isoweekday() % 7)
     
     # 获取在一起的日子的日期格式
     love_year = int(config["love_date"].split("-")[0])
