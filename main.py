84e6ed508c0131487821edf7f9eaf5864e880791
Betty Zhong <61925024+minionkevin@users.noreply.github.com>
2022-08-27 00:40:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -157,8 +157,8 @@ def get_greet_note(greet_note, time, isDayOff):
         greet_note = "终于休息咯，快回家躺在床上当个咸鱼吧"
     elif (23 <= time | time <= 1) & isDayOff == True:
         greet_note = "快起来玩游戏啦，当然也别忘了要好好休息，周末愉快，努力把把吃鸡"
-    elif (23 <= time | time <= 1) & isDayOff == False:
-        greet_note = "该睡觉啦，放下手机订好闹钟，明天还是打工人的一天，晚安"
+    #elif (23 <= time | time <= 1) & isDayOff == False:
+     #   greet_note = "该睡觉啦，放下手机订好闹钟，明天还是打工人的一天，晚安"
     else:
         greet_note = "今日份播报来啦"
     return greet_note
